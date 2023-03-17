6db75dfd82fc4de67ec4d55c5b017b950c50bfc6
tmccormack165 <60988784+tmccormack165@users.noreply.github.com>
2023-03-17 16:51:40 +0100

    added header file for non main functions

---
+++ Functions.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string nameForNumber (long number);
--- overloading.cpp
+++ overloading.cpp
@@ -3,39 +3,10 @@
 #include <ctime>
 #include <string>
 #include <vector>
+#include "Functions.h"
+
 using namespace std;
 
-vector<string> ones {"","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-vector<string> teens {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen","Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-vector<string> tens {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-
-string nameForNumber (long number) {
-    /**
-     * Generate the string literal for a given number using recursion. The function will operate
-     * for any integer less than 10^12
-     * 
-     *
-     * @param number The number that will be converted into a string literal
-     * @return the string literal of number ex 1 --> "One"
-     */
-
-    if (number < 10) {
-        return ones[number];
-    } else if (number < 20) {
-        return teens [number - 10];
-    } else if (number < 100) {
-        return tens[number / 10] + ((number % 10 != 0) ? " " + nameForNumber(number % 10) : "");
-    } else if (number < 1000) {
-        return nameForNumber(number / 100) + " Hundred" + ((number % 100 != 0) ? " " + nameForNumber(number % 100) : "");
-    } else if (number < 1000000) {
-        return nameForNumber(number / 1000) + " Thousand" + ((number % 1000 != 0) ? " " + nameForNumber(number % 1000) : "");
-    } else if (number < 1000000000) {
-        return nameForNumber(number / 1000000) + " Million" + ((number % 1000000 != 0) ? " " + nameForNumber(number % 1000000) : "");
-    } else if (number < 1000000000000) {
-        return nameForNumber(number / 1000000000) + " Billion" + ((number % 1000000000 != 0) ? " " + nameForNumber(number % 1000000000) : "");
-    }
-    return "error";
-}
 
 
 int main(int argc, char *argv[]){
@@ -131,5 +102,6 @@ int main(int argc, char *argv[]){
 
     return 0;
 }
+
 
 
